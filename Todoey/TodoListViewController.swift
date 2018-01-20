1b506ac98d67f34a4ed17429ee273700755cd401
Yuri Ramocan <yramocan@umich.edu>
2018-01-20 17:50:58 -0500

    Initialize repository with TodoListViewController

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Yuri Ramocan on 1/20/18.
+//  Copyright © 2018 Yuri Ramocan. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
